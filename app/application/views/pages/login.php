bb3d8e3bda147819b1ff72033bfda6255ec669c6
vitolike <victorcostaoliveira@gmail.com>
2022-07-19 02:44:49 -0300

    Initial Config

---
+++ app/application/views/pages/login.php
@@ -0,0 +1,52 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css" integrity="sha384-zCbKRCUGaJDkqS1kPbPd7TveP5iyJE0EjAuZQTgFLD2ylzuqKfdKlfG/eSrtxUkn" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.9.1/font/bootstrap-icons.css">
+
+    
+    <title>sparkEAM!</title>
+  </head>
+  <body class="bg-dark">
+
+    <div class="container-fluid p-5">
+
+    <div class="row">
+        <div class="col-sm">
+        
+            <div class="card shadow-sm mb-2 bg-white rounded mx-auto" style="width: 400px;">
+                <div class="card-body">
+                    <h5 class="card-title text-center mb-5"> Spark Enterprise Asset Management</h5>
+                    <form>
+                    <div class="form-group">
+                        <label for="exampleInputEmail1">Usuário</label>
+                        <input type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+                        <small id="emailHelp" class="form-text text-muted"><i class="bi bi-patch-exclamation"></i> Nunca compartilhe seus dados com ninguém.</small>
+                    </div>
+                    <div class="form-group">
+                        <label for="exampleInputPassword1">Senha</label>
+                        <input type="password" class="form-control" id="exampleInputPassword1">
+                    </div>
+                    <button type="submit" class="btn btn-primary btn-lg btn-block"><i class="bi bi-lightning-fill"></i> Entrar</button>
+                    </form>
+                </div>
+            </div>
+
+        </div>
+    </div>
+       
+    </div>
+
+
+
+
+    <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-fQybjgWLrvvRgtW6bFlB7jaZrFsaBXjsOMm/tB9LTS58ONXgqbR9W8oWht/amnpF" crossorigin="anonymous"></script>
+
+  </body>
+</html>
